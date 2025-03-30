c7052161f8ae4e967bf136254b8a78228a88fbc4
happen <1592578957@qq.com>
2025-03-30 23:11:09 +0800

    checkCodeMerge

---
+++ vars/checkCodeMerge.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+// 定义函数
+def checkCodeMerge(String beforeBranch, String currentBranch) {
+  def beforeBranchCommitHash = sh(
+          script: "git rev-parse origin/${beforeBranch}",
+          returnStdout: true
+  ).trim()
+  echo "Last commit on ${beforeBranch}: ${beforeBranchCommitHash}"
+
+  def contains = sh(
+          script: "git merge-base --is-ancestor  ${beforeBranchCommitHash}  origin/${params.CURRENT_BRANCH}  && echo true || echo false",
+          returnStdout: true
+  ).trim().toBoolean()
+  echo "${currentBranch} ${contains ? 'contains' : 'not contains'} ${beforeBranch}"
+  return contains
+}
