5833461ea19b587e6d8ead78e49abee3ef29de90
happen <1592578957@qq.com>
2025-04-06 09:30:09 +0800

    生成shell

--- vars/remoteShell.groovy
+++ vars/remoteShell.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(Object server, String servers_credentials_id, String remote_shell) {
-    withCredentials([sshUserPrivateKey(credentialsId: "$servers_credentials_id", usernameVariable: 'REMOTE_USER', keyFileVariable: 'SSH_KEY_FILE')]) {
-        sh("ssh -oStrictHostKeyChecking=no -i $SSH_KEY_FILE $REMOTE_USER@${server.host} \"${remote_shell}\"")
+    withCredentials([sshUserPrivateKey(credentialsId: "$servers_credentials_id", usernameVariable: 'REMOTE_USER', keyFileVariable: 'SSH_KEY_FILE')]
+            ,string(credentialsId: "$remote_shell", variable: 'REMOTE_SHELL')
+        ) {
+        sh("ssh -oStrictHostKeyChecking=no -i $SSH_KEY_FILE $REMOTE_USER@${server.host} \"${REMOTE_SHELL}\"")
     }
 }
